4d1dcd00d0088597be0910db721a28cc37efa411
Lamont <viniciusmessias52@gmail.com>
2025-06-11 11:26:39 -0300

    feat: implement bank account creation via terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Por favor, digite seu nome: ");
+        String nomeUsuario = userInput.nextLine();
+
+        System.out.print("Por favor, digite o número da agência: ");
+        String numeroDaAgencia = userInput.nextLine();
+
+        System.out.print("Por favor, digite o número da conta: ");
+        int numeroDaConta = userInput.nextInt();
+
+        System.out.print("Por favor, digite o seu saldo: ");
+        double saldoDaConta = userInput.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s ja esta disponivel para saque", nomeUsuario, numeroDaAgencia, numeroDaConta, saldoDaConta);
+
     }
 }
