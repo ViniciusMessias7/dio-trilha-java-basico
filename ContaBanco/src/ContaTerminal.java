54ab8ae7fffbefdafa6f8db320b33fd460ea8ed3
Lamont <viniciusmessias52@gmail.com>
2025-06-12 10:42:18 -0300

    refactor: improve user input handling with exception management

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -10,13 +11,31 @@ public class ContaTerminal {
         System.out.print("Por favor, digite o número da agência: ");
         String numeroDaAgencia = userInput.nextLine();
 
-        System.out.print("Por favor, digite o número da conta: ");
-        int numeroDaConta = userInput.nextInt();
+        int numeroDaConta;
+        while (true) {
+            try {
+                System.out.print("Por favor, digite o número da conta: ");
+                numeroDaConta = userInput.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                userInput.next();
+                System.out.println("\nIsso não é um número válido! Tente novamente.");
+            }
+        }
 
-        System.out.print("Por favor, digite o seu saldo: ");
-        double saldoDaConta = userInput.nextDouble();
+        double saldoDaConta;
+        while (true) {
+            try {
+                System.out.print("Por favor, digite o seu saldo: ");
+                saldoDaConta = userInput.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                userInput.next();
+                System.out.println("\nIsso não é um saldo válido! Tente novamente.");
+            }
+        }
 
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s ja esta disponivel para saque", nomeUsuario, numeroDaAgencia, numeroDaConta, saldoDaConta);
-
+        userInput.close();
     }
 }
